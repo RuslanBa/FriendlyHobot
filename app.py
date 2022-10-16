fc5c3266f644ac75fa9de9026925e9d60575e307
ruslanbagautdinov <smile-fun@yandex.ru>
2022-10-16 18:04:15 +0300

    new

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask
 import asyncio
+import socketserver
+import http.server
 from aiogram.utils import executor
 from aiogram import types
 from loader import bot, dp
 from schedular import scheduler_monday, scheduler_thursday
 
+from http import HTTPStatus
 
-# app = Flask(__name__)
-#
-#
-# @app.route('/')
-# def hello_world():
-#     return 'Hello'
-#
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        msg = 'Hello! you requested %s' % self.path
+        self.wfile.write(msg.encode())
+
 
 @dp.message_handler()
 async def answer(message: types.Message):
@@ -26,5 +29,7 @@ async def on_startup(_):
 
 if __name__ == '__main__':
     print('начинаем работу')
-    port = 8080
+    port = ('0.0.0.0', 80)
+    httpd = socketserver.TCPServer(('', port), Handler)
+    httpd.serve_forever()
     executor.start_polling(dp, on_startup=on_startup)
